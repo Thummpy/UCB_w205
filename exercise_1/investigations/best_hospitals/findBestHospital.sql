0f3f2e9364c56b7fc9d9c2284a46dfb6089e37f2
Thummpy <Thummpy@users.noreply.github.com>
2016-06-27 08:07:52 -0700

    Delete findBestHospital.sql

--- exercise_1/investigations/best_hospitals/findBestHospital.sql
+++
@@ -1,3 +0,0 @@
-select Provider_ID,(sum(Patient_Survey_Star_Rating* if(Number_of_Completed_Surveys = 'Between 100 and 299',150,300)) / sum(if(Number_of_Completed_Surveys = 'Between 100 and 299',150,300))) as weighted_score from survey_hospital_s where Patient_Survey_Star_Rating in (0,1,2,3,4,5) group by Provider_ID order by weighted_score DESC limit 100;
-
-
